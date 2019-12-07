0c7d5d65de93011a33813705fb45bcf04f764d3d
lzhnb <liang13268932888@outlook.com>
2019-12-07 12:44:20 +0800

    finish hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -2,37 +2,54 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cddr s))
 )
 
 (define (sign x)
-  'YOUR-CODE-HERE
+  (cond ((> x 0) 1)
+        ((= x 0) 0)
+        ((< x 0) -1)
+  )
 )
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
+  (cond ((zero? n) 1)
+        ((even? n) (square(pow b (/ n 2))))
+        ((odd? n) (* b (pow b (- n 1))))
+  )
 )
 
 (define (ordered? s)
-  'YOUR-CODE-HERE
+  (cond ((null? s) #t)
+        ((null? (cdr s)) #t)
+        ((< (car s) (cadr s)) (ordered? (cdr s)))
+        (else #f)
+  )
 )
 
 (define (empty? s) (null? s))
 
 (define (add s v)
-    'YOUR-CODE-HERE
+    (cond ((empty? s) (list v))
+          ((= (car s) v) s)
+          ((> (car s) v) (cons v s))
+          ((< (car s) v) (cons (car s) (add (cdr s) v)))
     )
+)
 
 ; Sets as sorted lists
 (define (contains? s v)
-    'YOUR-CODE-HERE
+    (cond ((empty? s) #f)
+          ((= (car s) v) #t)
+          (else (contains? (cdr s) v))
     )
+)
 
 ; Equivalent Python code, for your reference:
 ;
@@ -50,8 +67,12 @@
 ;         return contains(s.rest, v)
 
 (define (intersect s t)
-    'YOUR-CODE-HERE
+    (cond ((or (empty? s) (empty? t)) nil)
+        ((= (car s) (car t)) (cons (car s) (intersect (cdr s) (cdr t))))
+        ((< (car s) (car t)) (intersect (cdr s) t))
+        ((> (car s) (car t)) (intersect s (cdr t)))
     )
+)
 
 ; Equivalent Python code, for your reference:
 ;
@@ -68,5 +89,10 @@
 ;             return intersect(set1, set2.rest)
 
 (define (union s t)
-    'YOUR-CODE-HERE
+    (cond ((empty? s) t)
+        ((empty? t) s)
+        ((= (car s) (car t)) (cons (car s) (union (cdr s) (cdr t))))
+        ((< (car s) (car t)) (cons (car s) (union (cdr s) t)))
+        ((> (car s) (car t)) (cons (car t) (union s (cdr t))))
     )
+)
