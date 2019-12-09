5d96922e995d01b04c44893be1232ac4ea4ad53f
lzhnb <liang13268932888@outlook.com>
2019-12-09 10:40:31 +0800

    finish hw10

--- hw/hw10/hw10.scm
+++ hw/hw10/hw10.scm
@@ -1,16 +1,32 @@
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 0) start
+      (combiner (term n) (accumulate combiner start (- n 1) term)))
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (define (accumulate-iter n tail)
+    (if (= n 0) tail
+    (accumulate-iter (- n 1) (combiner (term n) tail))))
+  (accumulate-iter n start)
 )
 
 (define (partial-sums stream)
-  'YOUR-CODE-HERE
+  (define (helper sum stream)
+    (if (null? stream) nil
+        (cons-stream (+ sum (car stream)) (helper (+ sum (car stream)) (cdr-stream stream)))
+    )
+  )
   (helper 0 stream)
 )
 
 (define (rle s)
-  'YOUR-CODE-HERE
+  (define (helper prev n stream)
+    (cond ((null? stream) (cons-stream (list prev n) nil))
+          ((= prev (car stream)) (helper prev (+ n 1) (cdr-stream stream)))
+          (else (cons-stream (list prev n) (helper (car stream) 0 stream)))
+    )
+  )
+  (if (null? s) nil
+    (helper (car s) 0 s)
+  )
 )
